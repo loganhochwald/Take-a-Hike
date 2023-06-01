68ecd5a58c6f1fe62a332823132c85b00d9ae3ad
logan <loganh42@gmail.com>
2023-06-01 15:29:37 -0500

    Edit TradeDisplay to Contain Trade Image

--- client/components/TradingPost/TradeDisplay.jsx
+++ client/components/TradingPost/TradeDisplay.jsx
@@ -6,13 +6,17 @@ const TradeDisplay = () => {
 
   const [post, setPost] = useState(location.state?.thisPost);
 
+  //THE LOCATION MAP WILL NEED TO BE PLACED NEXT TO THIS SO THE CARD IS ON THE LEFT AND THE MAP IS ON THE RIGHT
   return (
-<div className='list-item-card'>
-  <h1>{post.title}</h1>
-  <h2>Price: {post.price}</h2>
-  <p>Location: {post.location}</p>
-  <p>{post.description}</p>
-</div>
+    <div className='list-item-card'>
+      <img src={post.pictures} style={{ width: '400px', height: '400px' }} />
+      <div>
+        <h1>{post.title}</h1>
+        <h2>Price: {post.price}</h2>
+        <p>Location: {post.location}</p>
+        <p>{post.description}</p>
+      </div>
+    </div>
   );
 }
 
